d204531437115de1abd1f267edc2dee67a33e06d
Ritesh <blitzritz@gmail.com>
2024-11-08 13:38:16 +0530

    fix

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,16 +1,16 @@
-import Authors from "@/components/Authors";
+"use client";
 import Books from "@/components/Books";
 import Tab from "@/components/Tab";
-import Image from "next/image";
-import { redirect } from "next/navigation";
-import { useState } from "react";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] min-h-screen px-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
       <main className="w-full h-full flex flex-col gap-8 row-start-2 sm:items-start">
         <Tab selectedTabId={"books"} />
-        <Books />
+        <Suspense>
+          <Books />
+        </Suspense>
       </main>
     </div>
   );
